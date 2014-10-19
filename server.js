8fadb72870abe84c0a6f6e173b709728ab6dc9b3
Vivek Chaudhary <vc1023@gmail.com>
2014-10-19 21:43:24 +0530

    added restify and routers , removed original express app in server.js

--- server.js
+++ server.js
@@ -2,158 +2,101 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
+var restify = require('restify');
+var mongoose = require("mongoose");
+
+var server = restify.createServer({
+    name : "appointment"
+});
+ 
+//var connection_string = 'mongodb://localhost:27017/optimus';
+var connection_string = 'mongodb://admin/xdSqqbpcK_-T@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/bumblebee'
+
+// Connect to mongodb
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  mongoose.connect(connection_string, options);
+};
+
+connect();
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+
+var Schema = mongoose.Schema;
+
+var OrganizationSchema = new Schema({
+                          org_id                : Number,
+                          name                  : String,
+                          address1              : String,
+                          address2              : String,
+                          city                  : String,
+                          state                 : String,
+                          zip                   : Number,
+                          country               : String,
+                          phone_number          : Number,
+                          latitude              : Number,
+                          longitude             : Number,
+                          image                 : String,
+                          service_type          : String,
+                          created_date          : Date,
+                          updated_date          : Date,
+                          created_by            : Number,
+                          updated_by            : Number
+                        })
+
+var Organization = mongoose.model("Organization",OrganizationSchema)
+
+var SchedulesSchema = new Schema({
+                          schedule_id       : Number,
+                          org_id            : Number,
+                          customer_id       : Number,
+                          specialist_id     : Number,
+                          appointment_date  : Date,
+                          start_time        : Date,
+                          end_time          : Date,
+                          created_date      : Date,
+                          updated_date      : Date,
+                          created_by        : Number,
+                          updated_by        : Number
+                          //date            : { type: Date, set: setDateWithFormat },
+                          //start_time  : { type: Date, set: setStartTimeWithFormat },
+                          //end_time        : { type: Date, set: setEndTimeWithFormat },
+                            
+                        })
+
+var Schedules = mongoose.model("Schedules",SchedulesSchema)
+
+ //  Set the environment variables we need.
+var nodejs_ip_address = process.env.OPENSHIFT_NODEJS_IP;
+var nodejs_port       = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+if (typeof nodejs_ip_address === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+    nodejs_ip_address = "127.0.0.1";
+};
+
+server.use(restify.queryParser())
+server.use(restify.bodyParser()) 
+server.listen(nodejs_port ,nodejs_ip_address, function(){
+    console.log('%s listening at %s ', server.name , server.url);
+})
+
+var SCHEDULE_PATH = '/schedules'
+
+server.get({path : SCHEDULE_PATH } , getAllSchedules) //get all schedules
+
+function getAllSchedules(req, res , next){
+   // console.log("in find all schedules" +  mongoose.connection)
+    res.setHeader('Access-Control-Allow-Origin','*')
+    Schedules.find(function (err, schedules) {
+          
+          if (err) return console.error(err)
+          res.json(schedules);
+    })
+}
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
-
-    //  Scope.
-    var self = this;
-
-
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index1.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index1.html'] = fs.readFileSync('./index1.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index1.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
 
