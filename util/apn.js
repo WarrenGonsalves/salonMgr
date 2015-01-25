e613077981de8bfe25178ae28e180b90a844da30
Naik, Parag Laxman (US - Mumbai) <parnaik@deloitte.com>
2015-01-25 17:12:08 +0530

    apn

--- util/apn.js
+++ util/apn.js
@@ -12,7 +12,9 @@ if (config.env === "prod") {
 
     options = {
         pfx: p12Path,
-        passphrase: "q"
+        passphrase: "q",
+        "batchFeedback": true,
+        "interval": 120
     };
 
 } else {
@@ -25,6 +27,13 @@ if (config.env === "prod") {
     };
 }
 
+var feedback = new apn.Feedback(options);
+feedback.on("feedback", function(devices) {
+    devices.forEach(function(item) {
+        logger.info(__filename, ["apn feedback"], JSON.stringify(item));
+    });
+});
+
 module.exports.sendAPN = function(token, pushPayload) {
 
     var myDevice = new apn.Device(token);
@@ -32,12 +41,12 @@ module.exports.sendAPN = function(token, pushPayload) {
     var apnConnection = new apn.Connection(options);
 
     note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-    note.badge = 3;
+    note.badge = 1;
     note.sound = "ping.aiff";
     note.alert = "\uD83D\uDCE7 \u2709 Handz invoice";
     note.payload = pushPayload;
 
-    logger.info("APN", ["APN for token", token], JSON.stringify(note))
+    logger.info("APN", ["APN for token", token, JSON.stringify(options)]);
 
     apnConnection.pushNotification(note, myDevice);
 }
