c8f13a211089240474318516be7e6a6157284075
Naik, Parag Laxman (US - Mumbai) <parnaik@deloitte.com>
2015-01-26 13:56:06 +0530

    Multiple GCM issue fix.

--- util/gcm.js
+++ util/gcm.js
@@ -2,8 +2,6 @@ var gcm = require('node-gcm');
 var logger = require('./logger');
 
 var message = new gcm.Message({
-    collapseKey: 'demo',
-    delayWhileIdle: true,
     timeToLive: 3,
     data: {
         sender: 'Fixers'
@@ -11,10 +9,10 @@ var message = new gcm.Message({
 });
 
 var sender = new gcm.Sender('AIzaSyCBOByaMFuG6v_hjeh2huS4mJtpHPXYncQ');
-var registrationIds = [];
 
 module.exports.sendGCM = function(token, invoice_id) {
 
+    var registrationIds = [];
     registrationIds.push(token);
     message.data.invoice_id = invoice_id;
 
