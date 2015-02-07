883c7a9f4dd8dbed64dc0efabc695fcf685bc86d
Naik, Parag Laxman (US - Mumbai) <parnaik@deloitte.com>
2015-02-07 13:00:25 +0530

    Specialist Referral:

--- routes/specialist.js
+++ routes/specialist.js
@@ -35,7 +35,7 @@ module.exports = function() {
      */
     {
       method: 'POST',
-      path: BASE_URL + '/{fname}/{cat}',
+      path: BASE_URL,
       config: specialistController.postConfigHandler
     },
     /**
