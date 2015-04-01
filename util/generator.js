5a8f0e247874f2ee484cea9937e48cca6a9edb64
Naik, Parag Laxman (US - Mumbai) <parnaik@deloitte.com>
2015-04-01 14:22:14 +0530

    generator: simple comment.

--- util/generator.js
+++ util/generator.js
@@ -3,6 +3,7 @@ var hat = require('hat');
 /**
  * Helper method to return a random UUID
  * @return - random id - 0c82a54f22f775a3ed8b97b2dea74036
+ * new comment added
  */
 module.exports.getUUID = function() {
     return hat();
